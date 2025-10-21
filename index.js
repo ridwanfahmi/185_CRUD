5b9c3a0716595e5cff8ee98977ea8d7e234fd49e
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-21 14:55:56 +0700

    Merevisi endpoint get

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ db.connect((err) => {
 });
 
 app.get('/api/mahasiswa', (req, res) => {
-    db.query('SELECT * from mahasiswa', (err, result) => {
+    db.query('SELECT * from biodata', (err, result) => {
         if (err) {
             console.error('Error executing query:' + err.stack);
             res.status(500).send('Error fetching users');
